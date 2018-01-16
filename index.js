8ad0817d089b0e3a297ceef3bb791515e1217597
Seth <Seth@Seths-iMac.local>
2018-01-16 12:07:06 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const data = require("./data.json");
+const cors = require("cors");
+
+function findById(data, id) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            return data[i]
+        }
+    } 
+    return null
+}
+
+const app = express();
+app.use(cors());
+
+app.get("/", function (request, response) {
+    response.json({data});
+});
+
+app.get("/:id", function(request, response) {
+    var record = findById(data, request.params.id);
+    if (!record) {
+        response.status(404).json({
+            error: {
+                message: "No Record Found!"
+            }
+        });
+    } else {
+        response.json({data: record});
+    }
+})
+app.listen(4000);
